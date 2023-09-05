2f386b12e748e5afb34d316c424491c58303cc29
shashark3 <2548947046@qq.com>
2023-09-05 17:36:51 +0800

    集成gin

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// 初始化 Gin 实例
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+	r.NoRoute(func(ctx *gin.Context) {
+		acceptString := ctx.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			ctx.String(http.StatusNotFound, "页面不存在，404 NOT FOUND")
+		} else {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认url和请求方法是否正确。",
+			})
+		}
+	})
+
+	// 运行服务
+	r.Run(":8000")
 }
